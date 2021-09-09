501c09140098b9339880ffef977e886b64193184
Ben <benjaminbotrosoutlook.com>
2021-09-09 09:41:49 +0200

    Element Toggle

--- client/src/components/dashboard/private.jsx
+++ client/src/components/dashboard/private.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import ElementToggle from './element-toggle/element-toggle';
+
 const Dashboard = (props) => {
 
   return(
     <div className="private">
-    private
+      <ElementToggle label={'users'} content={'hello'} />
+      <ElementToggle label={'carouselle'} content={'hello'} />
+      <ElementToggle label={'content'} content={'hello'} />
     </div>
   )
 }
