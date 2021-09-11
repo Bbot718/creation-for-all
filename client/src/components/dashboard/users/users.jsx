69bdc0410ad468b636aedb2aa520172aedc3e4d8
Ben <benjaminbotrosoutlook.com>
2021-09-11 12:07:14 +0200

    Page Selection

--- client/src/components/dashboard/users/users.jsx
+++ client/src/components/dashboard/users/users.jsx
@@ -1,11 +1,14 @@
 import React, {useEffect, useState, ref, useRef} from 'react';
 import Axios from 'axios';
 
+
 const Users = (props) => {
 
   const [users, setUsers] = useState([]);
   const [usernameReg, setUsernameReg] = useState("");
   const [passwordReg, setPasswordReg] = useState("");
+
+
 
   const usernameField = useRef();
   const passwordField = useRef();
@@ -29,31 +32,39 @@ const Users = (props) => {
   }
 
   const DeleteUser = (id) => {    
+    
     Axios
     .post('http://localhost:3001/delete_user', {id: id})
     .then((response) => {console.log(response)});
 
     props.logout();
+    
   }
 
   return(
-    <ul className="users-dashboard">
-      {users.map((user, i) => {
-        return(
-          <li key={user.login_id} className="users-dashboard__item">
-            <div className='bold-text' >{user.username}</div>
-            {(user.username === props.user) && <button onClick={() => DeleteUser(user.login_id)} className='regular-text users-dashboard__button' >{"delete"}</button>}
-          </li>
-        )
-      })}
-      <form >
-      <li className="users-dashboard__item">   
-          <input ref={usernameField} className='regular-text users-dashboard__input' placeholder='username' type="text" onChange={((e) => setUsernameReg(e.target.value))} />
-          <input ref={passwordField} className='regular-text users-dashboard__input' placeholder='password' type="password" onChange={((e) => setPasswordReg(e.target.value))} />
-          <button className='regular-text users-dashboard__button' onClick={Register}>{"add"}</button>       
-      </li>
-      </form>
-    </ul>
+    <React.Fragment>        
+      <ul className="users-dashboard">
+        {users.map((user, i) => {
+          return(
+            <li key={user.login_id} className="users-dashboard__item">
+              <div className='bold-text' >{user.username}</div>
+              {(user.username === props.user) && 
+                <button className='regular-text users-dashboard__button'  onClick={() => {
+                  if (window.confirm('Are you sure you wish to delete ' + user.username + '?')) {DeleteUser(user.login_id)}}}>{"delete"}
+                </button>}
+            </li>
+            )
+          })}
+        <form >
+        <li className="users-dashboard__item">   
+            <input ref={usernameField} className='regular-text users-dashboard__input' placeholder='username' type="text" onChange={((e) => setUsernameReg(e.target.value))} />
+            <input ref={passwordField} className='regular-text users-dashboard__input' placeholder='password' type="password" onChange={((e) => setPasswordReg(e.target.value))} />
+            <button className='regular-text users-dashboard__button' onClick={Register}>{"add"}</button>       
+        </li>
+        </form>
+      
+      </ul>
+    </React.Fragment>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -26,8 +25,6 @@ app.post('/login', (req, res) => {
         if(err){
             res.send({err: err})
         } 
-
-
         (result.length > 0) ? res.send(result) : res.send({message: "* Wrong Username or password"})  
     })
 })
@@ -53,11 +50,19 @@ app.post('/delete_user', (req, res) => {
 
     db.query("DELETE FROM T_LOGIN WHERE login_id = ?", 
     [id],
-    (err, result) => {
-        console.log(err);
-    })
+    (err, result) => {console.log(err);})
 })
 
+app.get('/pageWCarouselle', (req, res) => {
+    db.query("SELECT * FROM T_PAGE WHERE hasCarouselle = 1", 
+    (err, result) => {res.send(result);})
+})
+
+app.get('/page', (req, res) => {
+    db.query("SELECT * FROM T_PAGE", 
+    (err, result) => {res.send(result);})
+})
+ 
 app.listen(3001, () => {
     console.log('running on port 3001')
 });
