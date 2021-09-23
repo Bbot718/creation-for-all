a74b7a48b1bea0caeb4747169f1f10a239b6160b
Ben <benjaminbotrosoutlook.com>
2021-09-23 12:41:02 +0200

    Footer (Without links)

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import mail from '../../assets/icons/mail.png'
+import facebook from '../../assets/icons/facebook.png'
+import instagram from '../../assets/icons/instagram.png'
+
 const Footer = (props) => {
 
   return(
     <div className="footer">
-      footer
+      <img className="footer__image" src={mail} />
+      <img className="footer__image" src={facebook} />
+      <img className="footer__image" src={instagram} />
     </div>
   )
 }
